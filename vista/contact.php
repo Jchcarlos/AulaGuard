1611d2146109706fe7af52d9fa8ce1334fc602cd
Ghoodra <alanjosafhat1234@gmail.com>
2023-11-29 02:37:42 -0600

    Update contact.php

--- vista/contact.php
+++ vista/contact.php
@@ -48,7 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && $usuario_autenticado) {
     <link rel="stylesheet" href="recursos/css/productos.css">
     <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
-
+    
 </head>
 
 <body>
@@ -113,5 +113,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && $usuario_autenticado) {
     ?>
     <script src="https://unpkg.com/scrollreveal"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 </body>
 </html>
