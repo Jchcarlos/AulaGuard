d0ab2c5fb64c3a19d2cac9220a06ef2728146f94
Ghoodra <alanjosafhat1234@gmail.com>
2023-11-29 02:17:48 -0600

    Update login.php

--- vista/login.php
+++ vista/login.php
@@ -2,13 +2,19 @@
     //Metodo Session iniciada
     session_start();
 	 
-	if(isset($_SESSION['usuario_email'])){
-		header("location : ../index.php");
+
+	if (isset($_SESSION['usuario_email']) && $_SESSION['usuario_rol'] === 'administrador') {
+		echo '
+			<script>
+				alert("No tienes permiso para acceder a esta página.");
+				window.location = "admin/index.php";
+			</script>
+		';
+		exit();
 	}
-	
 ?>
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 
 <head>
 	<meta charset="UTF-8" />
@@ -20,8 +26,9 @@
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet"/>
 	<link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
-	<link rel="stylesheet" href="recursos/css/login.css" />
-	    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+	<link rel="stylesheet" href="recursos/css/login.css"/>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    
 </head>
 
 <body>
@@ -60,5 +67,4 @@
 	<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
-
 </html>
